ba444dcbc80d62fbf493bfae98c9b234d43fabda
iLouis1 <rhiganakatugba@gmail.com>
2023-02-19 17:54:16 -0800

    Territory dont make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 /**
- * main -main function
- *
- * Return: o on success
+ * main - prints the size of various types a base on
+ * the computer it is compiled and run on..
+ * Return: Always 0.
  */
 
 int main(void)
+
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
+}
