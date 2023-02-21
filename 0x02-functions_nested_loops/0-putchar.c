f06043a016553142b855a169c41785cac5399245
iLouis1 <rhiganakatugba@gmail.com>
2023-02-21 11:17:31 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 /**
  * main - main function
  * _putchar: print the alphabets putchar
- * - Return: always 0
+ * Return: always 0
  */
 
 int main(void)
 {
-	_putchar(',');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,5 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
