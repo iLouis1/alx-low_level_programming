d098b96687f04719656ab7047fb92e5ed4834539
iLouis1 <rhiganakatugba@gmail.com>
2023-02-27 15:59:17 -0800

    Don't swap

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * reset_t0_98 - This takes a pointer to an int and updates
+ * reset_to_98 - This takes a pointer to an int and updates
  * the value it points to 98
  *
  * @n: An integer input pointer
