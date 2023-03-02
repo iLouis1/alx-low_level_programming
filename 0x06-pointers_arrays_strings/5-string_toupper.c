53b8cf01a4d227e504935a6f699c362ca3a8b4b7
iLouis1 <rhiganakatugba@gmail.com>
2023-03-02 15:22:12 -0800

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @n: pointer
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int b;
+
+	b = 0;
+	while (n[b] != '\0)
+	{
+		if (n[b] >= 'a' && n[b] <= 'z')
+			n[b] = [b] - 32;
+		b++;
+	}
+	return (n);
+}
