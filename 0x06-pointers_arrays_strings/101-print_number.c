5f152efb5123ea323fd826825cc6839c896d79c4
iLouis1 <rhiganakatugba@gmail.com>
2023-03-02 19:23:55 -0800

    symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
 * print_number - prints an integer
 * @n:integer to be printed
 * Return: void
 */
+
 void print_number(int n)
+
 {
 	unsigned int num;
 
