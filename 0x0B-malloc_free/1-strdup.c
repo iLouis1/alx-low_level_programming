1d037c4fa27658dd7373e24e27cf6b6f5874fc58
iLouis1 <rhiganakatugba@gmail.com>
2023-03-13 18:15:35 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
-  * _strdup - This duplicates a string
-  * @str: The string to duplicate
-  * 
-  * Return: String duplicated
-  */
+ * _strdup - This duplicates a string
+ * @str: The string to duplicate
+ *
+ * Return: This is the string duplicated
+ */
 
 char *_strdup(char *str)
 
@@ -21,7 +22,7 @@ char *_strdup(char *str)
 		i++;
 	}
 
-	s = malloc((sizeof(char) * i) + i);
+	s = malloc((sizeof(char) * i) + 1);
 
 	if (s == NULL)
 		return (NULL);
