280bc43aa69273241278883f0191e0d681164c50
iLouis1 <rhiganakatugba@gmail.com>
2023-03-13 17:43:42 -0700

    Float like a butterfly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,12 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	
-       s = malloc(size * sizeof(char));
-	
-       if (s == NULL)
-       return (NULL);
 
-       for (i = 0; i < size; i++)
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+	return (NULL);
+
+	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
 	}
