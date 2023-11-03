6c91032afce26ac00976f1e18b136cfbf8c8fa95
iLouis1 <rhiganakatugba@gmail.com>
2023-11-03 01:01:43 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,33 @@
 #include "main.h"
 
 /**
- * read_textfile - This will print the letters and read a text file
- * @filename: filename.
- * @letters: The numbers of letters printed.
+ * read_textfile - This reads a text file and prints the letters
+ * @filename: The filename.
+ * @letters: These are numbers of letters printed.
  *
- * Return: It return numbers of letters printed. will return 0, If fails.
+ * Return: It returns numbers of letters printed. If fails, returns 0.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char*filename, size_t letters)
 {
-	if (!filename)
-		return (-1);
-	int fd = open(filename, O_RDONLY);
-		if (fd == -1)
-		{
-			return (-1);
-		}
-
-char *buf = malloc(sizeof(char) * letters);
-if (!buf)
-	{
-	close(fd);
-	return (-1);
-	}
-
-ssize_t nrd = read(fd, buf, letters);
-if (nrd == -1)
-	{
-		close(fd);
-		free(buf);
-		return (-1);
-	}
-
-ssize_t nwr = write(STDOUT_FILENO, buf, nrd);
-if (nwr == -1)
-	{
-		close(fd);
-		free(buf);
-		return (-1);
-	}
-
-close(fd);
+if(!filename)
+	{return-1;}
+int fd=open
+	(filename,O_RDONLY);
+if(fd==-1)
+	{return-1;}
+char*buf=malloc(letters);
+if(!buf){close(fd);
+	return-1;}
+ssize_t nrd=read
+	(fd,buf,letters);
+if(nrd==-1)
+	{free(buf);close(fd);
+	return-1;}
+ssize_t nwr=write(STDOUT_FILENO,buf,nrd);
+if(nwr==-1||nwr!=nrd){free(buf);close(fd);
+	return-1;}
 free(buf);
-
+close(fd);
 return (nwr);
 }
