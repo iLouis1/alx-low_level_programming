1ed7dfa3c4e6630422919a3cab871dc3a1baea83
iLouis1 <rhiganakatugba@gmail.com>
2023-11-03 16:05:45 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,33 +1,51 @@
 #include "main.h"
 
 /**
- * read_textfile - This reads a text file and prints the letters
- * @filename: The filename.
- * @letters: These are numbers of letters printed.
+ * read_textfile - This will read a text file and print the letters
+ * @filename: Tis is the filename.
+ * @letters: The numbers of letters printed.
  *
- * Return: It returns numbers of letters printed. If fails, returns 0.
+ * Return: Will return numbers of letters printed. Returns 0, If fails.
  */
 
-ssize_t read_textfile(const char*filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-if(!filename)
-	{return-1;}
-int fd=open
-	(filename,O_RDONLY);
-if(fd==-1)
-	{return-1;}
-char*buf=malloc(letters);
-if(!buf){close(fd);
-	return-1;}
-ssize_t nrd=read
-	(fd,buf,letters);
-if(nrd==-1)
-	{free(buf);close(fd);
-	return-1;}
-ssize_t nwr=write(STDOUT_FILENO,buf,nrd);
-if(nwr==-1||nwr!=nrd){free(buf);close(fd);
-	return-1;}
-free(buf);
-close(fd);
-return (nwr);
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+	return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+
+	nrd = read(fd, buf, letters);
+	if (nrd == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	nwr = write(STDOUT_FILENO, buf, nrd);
+	if (nwr == -1)
+	{
+	free(buf);
+	close(fd);
+	return (0);
+	}
+
+	close(fd);
+	free(buf);
+
+	return (nwr);
 }
