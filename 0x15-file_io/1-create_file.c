387e283f664685c508e50e73f17159c59482e9f1
iLouis1 <rhiganakatugba@gmail.com>
2023-11-03 17:08:38 -0700

    1. Under the sun

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,42 +1,39 @@
 #include "main.h"
 
 /**
- * create_file - Will create a file
- * @filename: This is the filename.
+ * create_file - Will create files
+ * @filename: The filename.
  * @text_content: The file in which content is written.
  *
- * Return: -1 if it fails, 1 if it succeeds.
+ * Return: 1 if it success. -1 if it fails.
  */
 
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	int nletters;
+	int rwr;
+
 	if (!filename)
-	return (-1);
+		return (-1);
 
-	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
-	return (-1);
+		return (-1);
 
 	if (!text_content)
-	text_content = "";
+		text_content = "";
 
-	int nletters = 0;
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
 
-	while
-		(text_content[nletters])
+	rwr = write(fd, text_content, nletters);
 
-	nletters++;
-
-		int rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-	{
-		close(fd);
+	if (rwr == -1)
 		return (-1);
-	}
 
 	close(fd);
 
 	return (1);
 }
+
